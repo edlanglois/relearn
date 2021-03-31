624a45427169401a5529954461e8fd4f04fa7256
Eric Langlois <eric@langlois.xyz>
2021-03-31 01:17:31 -0400

    Fix tests

--- src/envs/testing.rs
+++ src/envs/testing.rs
@@ -11,7 +11,7 @@ pub fn run_stateless<E>(env: E, num_steps: u64, seed: u64)
 where
     E: Environment,
     <E as Environment>::ObservationSpace: Debug,
-    <<E as Environment>::ObservationSpace as Space>::Element: Debug,
+    <<E as Environment>::ObservationSpace as Space>::Element: Debug + Clone,
     <E as Environment>::ActionSpace: Debug,
     <<E as Environment>::ActionSpace as Space>::Element: Debug,
 {
@@ -23,7 +23,7 @@ pub fn run_stateful<E>(env: &mut E, mut num_steps: u64, seed: u64)
 where
     E: StatefulEnvironment,
     <E as StatefulEnvironment>::ObservationSpace: Debug,
-    <<E as StatefulEnvironment>::ObservationSpace as Space>::Element: Debug,
+    <<E as StatefulEnvironment>::ObservationSpace as Space>::Element: Debug + Clone,
     <E as StatefulEnvironment>::ActionSpace: Debug,
     <<E as StatefulEnvironment>::ActionSpace as Space>::Element: Debug,
 {
@@ -44,7 +44,7 @@ where
     simulation::run(env, &mut agent, |step| {
         assert!(step.reward >= min_reward);
         assert!(step.reward <= max_reward);
-        if let Some(obs) = step.next_observation {
+        if let Some(obs) = &step.next_observation {
             assert!(observation_space.contains(obs))
         } else {
             assert!(step.episode_done);
