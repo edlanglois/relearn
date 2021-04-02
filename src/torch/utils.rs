b48fc4195bbc8e7248194984d3c297ac8d647f05
Eric Langlois <eric@langlois.xyz>
2021-04-02 17:38:58 -0400

    Unit tests for torch::utils::one_hot

--- src/torch/utils.rs
+++ src/torch/utils.rs
@@ -27,3 +27,66 @@ pub fn f_one_hot(labels: &Tensor, num_classes: usize, kind: Kind) -> Result<Tens
 pub fn one_hot(labels: &Tensor, num_classes: usize, kind: Kind) -> Tensor {
     f_one_hot(labels, num_classes, kind).unwrap()
 }
+
+#[cfg(test)]
+mod one_hot {
+    use super::*;
+
+    #[test]
+    fn scalar_i32() {
+        assert_eq!(
+            one_hot(&Tensor::from(1i64), 4, Kind::Int),
+            Tensor::of_slice(&[0, 1, 0, 0])
+        );
+    }
+
+    #[test]
+    fn scalar_i64() {
+        assert_eq!(
+            one_hot(&Tensor::from(1i64), 4, Kind::Int64),
+            Tensor::of_slice(&[0, 1, 0, 0])
+        );
+    }
+
+    #[test]
+    fn scalar_f32() {
+        assert_eq!(
+            one_hot(&Tensor::from(3i64), 4, Kind::Float),
+            Tensor::of_slice(&[0.0, 0.0, 0.0, 1.0f32])
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn scalar_from_f32_fails() {
+        let _ = one_hot(&Tensor::from(1.0f32), 4, Kind::Float);
+    }
+
+    #[test]
+    #[should_panic]
+    fn scalar_index_too_big_fails() {
+        let _ = one_hot(&Tensor::from(4i64), 4, Kind::Float);
+    }
+
+    #[test]
+    #[should_panic]
+    fn scalar_index_negative_fails() {
+        let _ = one_hot(&Tensor::from(-1i64), 4, Kind::Float);
+    }
+
+    #[test]
+    fn one_dim() {
+        assert_eq!(
+            one_hot(&Tensor::of_slice(&[2i64, 1]), 3, Kind::Int),
+            Tensor::of_slice(&[0, 0, 1, 0, 1, 0]).view((2, 3))
+        );
+    }
+
+    #[test]
+    fn two_dims() {
+        assert_eq!(
+            one_hot(&Tensor::of_slice(&[2i64, 1]).view((1, 2)), 3, Kind::Int),
+            Tensor::of_slice(&[0, 0, 1, 0, 1, 0]).view((1, 2, 3))
+        );
+    }
+}
