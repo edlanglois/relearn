65257a45bdf90ffffc6e09497636f5f71f91e991
Eric Langlois <eric@langlois.xyz>
2021-04-02 21:00:39 -0400

    seq_modules::rnn tests

--- src/torch/seq_modules/rnn.rs
+++ src/torch/seq_modules/rnn.rs
@@ -3,7 +3,7 @@ use super::{IterativeModule, SequenceModule};
 use tch::{nn::RNN, IndexOp, Tensor};
 
 /// Wrapper that implements the Sequence/Iterative Module tratis for [tch::nn::RNN].
-struct SeqModRNN<R: RNN>(R);
+pub struct SeqModRNN<R: RNN>(R);
 
 impl<R: RNN> SequenceModule for SeqModRNN<R> {
     fn seq_serial(&self, inputs: &Tensor, seq_lengths: &[usize]) -> Tensor {
@@ -41,5 +41,70 @@ impl<R: RNN> IterativeModule for SeqModRNN<R> {
 impl<R: RNN> From<R> for SeqModRNN<R> {
     fn from(rnn: R) -> Self {
         SeqModRNN(rnn)
+    }
+}
+
+#[cfg(test)]
+mod sequence_module {
+    use super::*;
+    use tch::{kind::Kind, nn, Device};
+
+    #[test]
+    fn gru_seq_serial() {
+        let in_features = 3;
+        let out_features = 2;
+        let batch_size = 4;
+        let vs = nn::VarStore::new(Device::Cpu);
+        let gru = SeqModRNN::from(nn::gru(
+            &vs.root(),
+            in_features,
+            out_features,
+            Default::default(),
+        ));
+
+        let seq_lengths = [1usize, 3, 2];
+        let total_seq_length: usize = seq_lengths.iter().sum();
+        let inputs = Tensor::ones(
+            &[batch_size, total_seq_length as i64, in_features],
+            (Kind::Float, Device::Cpu),
+        );
+        let output = gru.seq_serial(&inputs, &seq_lengths);
+        // Check shape
+        assert_eq!(
+            output.size(),
+            vec![batch_size, total_seq_length as i64, out_features]
+        );
+        // Sequences: 0 | 1 2 3 | 4 5
+        // Compare the inner sequences. The RNN should reset for each.
+        assert_eq!(output.i((.., 0, ..)), output.i((.., 1, ..)));
+        assert_eq!(output.i((.., 1..3, ..)), output.i((.., 4..6, ..)));
+    }
+}
+
+#[cfg(test)]
+mod iterative_module {
+    use super::*;
+    use tch::{kind::Kind, nn, Device};
+
+    #[test]
+    fn gru() {
+        let in_features = 3;
+        let out_features = 2;
+        let batch_size = 4;
+        let vs = nn::VarStore::new(Device::Cpu);
+        let gru = SeqModRNN::from(nn::gru(
+            &vs.root(),
+            in_features,
+            out_features,
+            Default::default(),
+        ));
+
+        let state = gru.initial_state(batch_size);
+        let input = Tensor::ones(
+            &[batch_size as i64, in_features],
+            (Kind::Float, Device::Cpu),
+        );
+        let (output, _) = gru.step(&input, &state);
+        assert_eq!(output.size(), vec![batch_size as i64, out_features]);
     }
 }
