3206b046ebbd2ef91ed20f9d2f4eca0bab4a1f81
Eric Langlois <eric@langlois.xyz>
2021-04-09 17:18:48 -0400

    Defs comment

--- src/defs/mod.rs
+++ src/defs/mod.rs
@@ -1,4 +1,7 @@
 //! Definition structures
+//!
+//! Each definition is a single type that can specify any of many different types implementing
+//! a trait. These types allow the agent and environment to be specified at runtime.
 mod agent;
 pub mod env;
 mod optimizer;
