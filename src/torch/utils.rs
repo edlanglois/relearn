127eaa9be911e376856445965c521636b19ef1c9
Eric Langlois <eric@langlois.xyz>
2021-05-04 14:39:25 -0400

    Add (un)flatten_tensors() to src/torch/utils

--- src/torch/utils.rs
+++ src/torch/utils.rs
@@ -1,4 +1,5 @@
 //! Torch utilities.
+use std::borrow::Borrow;
 use tch::{kind::Kind, TchError, Tensor};
 
 /// Create a one-hot tensor from a tensor of indices,
@@ -26,6 +27,61 @@ pub fn f_one_hot(labels: &Tensor, num_classes: usize, kind: Kind) -> Result<Tens
 ///
 pub fn one_hot(labels: &Tensor, num_classes: usize, kind: Kind) -> Tensor {
     f_one_hot(labels, num_classes, kind).unwrap()
+}
+
+/// Flatten a set of tensors into a single vector.
+pub fn f_flatten_tensors<I>(tensors: I) -> Result<Tensor, TchError>
+where
+    I: IntoIterator,
+    <I as IntoIterator>::Item: Borrow<Tensor>,
+{
+    Tensor::f_cat(
+        &tensors
+            .into_iter()
+            .map(|t| t.borrow().f_flatten(0, -1))
+            .collect::<Result<Vec<_>, _>>()?,
+        0,
+    )
+}
+
+/// Flatten a set of tensors into a single tensor.
+pub fn flatten_tensors<I>(tensors: I) -> Tensor
+where
+    I: IntoIterator,
+    <I as IntoIterator>::Item: Borrow<Tensor>,
+{
+    f_flatten_tensors(tensors).unwrap()
+}
+
+/// The number of elements in a Tensor with the given shape.
+///
+/// # Panics
+/// If any dimension has negative size.
+fn shape_size(shape: &[i64]) -> i64 {
+    assert!(
+        shape.iter().all(|&d| d >= 0),
+        "Negative dimension in shape {:?}",
+        shape
+    );
+    shape.iter().product()
+}
+
+/// Unflatten a vector into a set of tensors with the given shapes.
+///
+/// # Panics
+/// Panics if any shape has a dimension with negative size.
+pub fn f_unflatten_tensors(vector: &Tensor, shapes: &[Vec<i64>]) -> Result<Vec<Tensor>, TchError> {
+    let sizes: Vec<_> = shapes.iter().map(|shape| shape_size(shape)).collect();
+    vector
+        .f_split_with_sizes(&sizes, 0)?
+        .iter()
+        .zip(shapes)
+        .map(|(t, shape)| t.f_reshape(&shape))
+        .collect()
+}
+
+pub fn unflatten_tensors(vector: &Tensor, shapes: &[Vec<i64>]) -> Vec<Tensor> {
+    f_unflatten_tensors(vector, shapes).unwrap()
 }
 
 #[cfg(test)]
@@ -88,5 +144,30 @@ mod one_hot {
             one_hot(&Tensor::of_slice(&[2i64, 1]).view((1, 2)), 3, Kind::Int),
             Tensor::of_slice(&[0, 0, 1, 0, 1, 0]).view((1, 2, 3))
         );
+    }
+}
+
+#[cfg(test)]
+mod flatten {
+    use super::*;
+
+    #[test]
+    fn test_flatten_tensors() {
+        let a = Tensor::of_slice(&[1, 2, 3, 4, 5, 6]).reshape(&[2, 3]);
+        let b = Tensor::of_slice(&[10, 11, 12, 13]).reshape(&[4, 1, 1]);
+
+        let v = flatten_tensors(&[a, b]);
+        assert_eq!(v, Tensor::of_slice(&[1, 2, 3, 4, 5, 6, 10, 11, 12, 13]));
+    }
+
+    #[test]
+    fn test_unflatten_tensors() {
+        let v = Tensor::of_slice(&[1, 2, 3, 4, 5, 6, 10, 11, 12, 13]);
+        let shapes = [vec![2, 3], vec![4, 1, 1]];
+        let ts = unflatten_tensors(&v, &shapes);
+
+        let a = Tensor::of_slice(&[1, 2, 3, 4, 5, 6]).reshape(&[2, 3]);
+        let b = Tensor::of_slice(&[10, 11, 12, 13]).reshape(&[4, 1, 1]);
+        assert_eq!(ts, vec![a, b]);
     }
 }
