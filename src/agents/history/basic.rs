b6efe0ea7ae4e93e53877388861cdffc6ee07e75
Eric Langlois <eric@langlois.xyz>
2021-08-09 13:30:43 -0400

    History buffer interface & basic single-thread buffer

---
+++ src/agents/history/basic.rs
@@ -0,0 +1,65 @@
+use super::super::Step;
+use super::{HistoryBuffer, HistoryBufferSteps};
+use std::vec;
+
+/// Configuration for [`EpisodeBuffer`].
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct EpisodeBufferConfig {
+    pub ep_done_step_threshold: usize,
+    pub step_threshold: usize,
+}
+
+impl Default for EpisodeBufferConfig {
+    fn default() -> Self {
+        Self {
+            ep_done_step_threshold: 10_000,
+            step_threshold: 11_000,
+        }
+    }
+}
+
+/// Episode-based step history buffer
+///
+/// The buffer has a two step readyness thresholds in terms of number
+/// of collected steps: `ep_done_step_threshold` and `step_threshold`.
+#[derive(Debug, Clone)]
+pub struct EpisodeBuffer<O, A> {
+    /// The buffer is ready if the most recent episode is complete and
+    /// the total number of steps is at least `ep_done_step_threshold`.
+    pub ep_done_step_threshold: usize,
+
+    /// The buffer is ready if the total number of steps is at least `step_threshold`.
+    /// Setting this >= `ep_done_step_threshold` means that the buffer prefers to ready itself at
+    /// an episode boundary but will eventually be ready even if the episode never ends.
+    pub step_threshold: usize,
+
+    buffer: Vec<Step<O, A>>,
+}
+
+impl<O, A> From<EpisodeBufferConfig> for EpisodeBuffer<O, A> {
+    fn from(config: EpisodeBufferConfig) -> Self {
+        Self {
+            ep_done_step_threshold: config.ep_done_step_threshold,
+            step_threshold: config.step_threshold,
+            buffer: Vec::new(),
+        }
+    }
+}
+
+impl<O: 'static, A: 'static> HistoryBuffer<O, A> for EpisodeBuffer<O, A> {
+    fn push(&mut self, step: Step<O, A>) -> bool {
+        let episode_done = step.episode_done;
+        self.buffer.push(step);
+        let num_steps = self.buffer.len();
+        (episode_done && num_steps >= self.ep_done_step_threshold)
+            || (num_steps >= self.step_threshold)
+    }
+}
+
+impl<'a, O: 'a, A: 'a> HistoryBufferSteps<'a, O, A> for EpisodeBuffer<O, A> {
+    type StepIter = vec::Drain<'a, Step<O, A>>;
+
+    fn drain_steps(&'a mut self) -> Self::StepIter {
+        self.buffer.drain(..)
+    }
+}
--- src/agents/history/mod.rs
+++ src/agents/history/mod.rs
@@ -1,14 +1,29 @@
 //! History buffers
+mod basic;
+
 use super::super::Step;
 
 /// A step history buffer.
-pub trait HistoryBuffer<O, A> {
-    type StepIter: Iterator<Item = Step<O, A>>;
-
+pub trait HistoryBuffer<O, A>: for<'a> HistoryBufferSteps<'a, O, A> {
     /// Push a new step into the buffer.
     ///
     /// Steps must be pushed consecutively within each episode.
+    ///
+    /// Returns a Boolean indicating whether the buffer is ready to be drained for a model update.
     fn push(&mut self, step: Step<O, A>) -> bool;
+}
+
+/// History buffer steps interface.
+///
+/// This takes a lifetime to work around the fact that associated types cannot yet have lifetimes.
+///
+/// TODO: Fix once generic associated types are stabilized.
+pub trait HistoryBufferSteps<'a, O, A> {
+    type StepIter: Iterator<Item = Step<O, A>>;
+
     /// Drain the collected step history into an iterator of steps.
-    fn drain_steps(&self) -> Self::StepIter;
+    ///
+    /// It is allowed to drain the buffer at any time, but the amount of available data may not
+    /// meet the buffer's criteria unless the last call to `push` returned true.
+    fn drain_steps(&'a mut self) -> Self::StepIter;
 }
