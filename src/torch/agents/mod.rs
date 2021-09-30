3f2e594b073c4a8c3351312a76ce891fc59ab375
Eric Langlois <eric@langlois.xyz>
2021-09-30 16:02:52 -0400

    Remove ActorCriticBoxedAgent

--- src/torch/agents/mod.rs
+++ src/torch/agents/mod.rs
@@ -4,19 +4,3 @@ mod actor_critic;
 mod tests;
 
 pub use actor_critic::{ActorCriticAgent, ActorCriticConfig};
-
-use super::{
-    critic::Critic,
-    policy::Policy,
-    updaters::{UpdateCritic, UpdatePolicy},
-};
-
-/// Actor critic agent with boxed components.
-pub type ActorCriticBoxedAgent<OS, AS> = ActorCriticAgent<
-    OS,
-    AS,
-    Box<dyn Policy>,
-    Box<dyn UpdatePolicy<AS>>,
-    Box<dyn Critic>,
-    Box<dyn UpdateCritic>,
->;
