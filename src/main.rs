646bbfea0f68c9502feec48f4b4454cff3546336
Eric Langlois <eric@langlois.xyz>
2021-10-14 22:19:27 -0400

    main.rs helpers return the simulator

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,22 @@ use clap::Clap;
 use relearn::cli::Options;
 use relearn::defs::{boxed_multithread_simulator, boxed_serial_simulator, HooksDef};
 use relearn::logging::CLILoggerConfig;
-use relearn::simulation::MultithreadSimulatorConfig;
+use relearn::simulation::{MultithreadSimulatorConfig, Simulator};
 use relearn::{AgentDef, EnvDef, MultithreadAgentDef};
 use std::convert::From;
 use std::error::Error;
 
-fn run_serial(opts: &Options, env_def: EnvDef, hook_def: HooksDef) -> Result<(), Box<dyn Error>> {
+fn run_serial(
+    opts: &Options,
+    env_def: EnvDef,
+    hook_def: HooksDef,
+) -> Result<Box<dyn Simulator>, Box<dyn Error>> {
     let agent_def = Option::<AgentDef>::from(opts)
         .ok_or_else(|| format!("Agent {} is not single-threaded", opts.agent))?;
     println!("Agent:\n{:#?}", agent_def);
 
-    let env_seed = opts.seed;
-    let agent_seed = opts.seed.wrapping_add(1);
     let simulator = boxed_serial_simulator(env_def, agent_def, hook_def);
-    let logger_config = CLILoggerConfig::default();
-    let mut logger = logger_config.build_logger();
-    simulator
-        .run_simulation(env_seed, agent_seed, &mut logger)
-        .expect("Simulation failed");
-    Ok(())
+    Ok(simulator)
 }
 
 fn run_multithread(
@@ -28,7 +25,8 @@ fn run_multithread(
     env_def: EnvDef,
     hook_def: HooksDef,
     mut num_threads: usize,
-) -> Result<(), Box<dyn Error>> {
+    logger_config: CLILoggerConfig,
+) -> Result<Box<dyn Simulator>, Box<dyn Error>> {
     let agent_def = Option::<MultithreadAgentDef>::from(opts)
         .ok_or_else(|| format!("Agent {} is not multi-threaded", opts.agent))?;
     println!("Agent:\n{:#?}", agent_def);
@@ -45,16 +43,9 @@ fn run_multithread(
         num_threads
     );
 
-    let env_seed = opts.seed;
-    let agent_seed = opts.seed.wrapping_add(1);
-    let logger_config = CLILoggerConfig::default();
-    let mut logger = logger_config.build_logger();
     let simulator =
         boxed_multithread_simulator(sim_config, env_def, agent_def, hook_def, logger_config);
-    simulator
-        .run_simulation(env_seed, agent_seed, &mut logger)
-        .expect("Simulation failed");
-    Ok(())
+    Ok(simulator)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -66,9 +57,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     let hooks_def = HooksDef::from(&opts);
     println!("Simulation Hooks:\n{:#?}", hooks_def);
 
-    if let Some(num_threads) = opts.parallel_threads {
-        run_multithread(&opts, env_def, hooks_def, num_threads)
+    let logger_config = CLILoggerConfig::default();
+    let mut logger = logger_config.build_logger();
+
+    let simulator = if let Some(num_threads) = opts.parallel_threads {
+        run_multithread(&opts, env_def, hooks_def, num_threads, logger_config)?
     } else {
-        run_serial(&opts, env_def, hooks_def)
-    }
+        run_serial(&opts, env_def, hooks_def)?
+    };
+
+    let env_seed = opts.seed;
+    let agent_seed = opts.seed.wrapping_add(1);
+
+    simulator.run_simulation(env_seed, agent_seed, &mut logger)?;
+    Ok(())
 }
