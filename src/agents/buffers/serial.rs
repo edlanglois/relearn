b15fd6fe42deb19d800e0976fac3af374b5d560a
Eric Langlois <eric@langlois.xyz>
2021-11-02 15:29:07 -0400

    Add unit tests for SerialBuffer
    Fix off-by-one error in episode_ends

--- src/agents/buffers/serial.rs
+++ src/agents/buffers/serial.rs
@@ -37,7 +37,7 @@ pub struct SerialBuffer<O, A> {
 
     /// Steps from all episodes with each episode stored contiguously
     steps: Vec<Step<O, A>>,
-    /// The end index of each episode within `steps`.
+    /// One past the end index of each episode within `steps`.
     episode_ends: Vec<usize>,
 }
 
@@ -69,7 +69,7 @@ impl<O, A> SerialBuffer<O, A> {
         self.steps.push(step);
         let num_steps = self.steps.len();
         if episode_done {
-            self.episode_ends.push(num_steps - 1)
+            self.episode_ends.push(num_steps)
         }
         (episode_done && num_steps >= self.soft_threshold) || (num_steps >= self.hard_threshold)
     }
@@ -174,5 +174,148 @@ impl<'a, O: 'a, A: 'a> HistoryBufferEpisodes<'a, O, A> for SerialBuffer<O, A> {
             &self.steps,
             self.episode_ends.iter().cloned().chain(incomplete_end),
         )
+    }
+}
+
+#[allow(clippy::needless_pass_by_value)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::{fixture, rstest};
+
+    /// Make a step that either continues or is terminal.
+    const fn step(observation: usize, next_observation: Option<usize>) -> Step<usize, bool> {
+        Step {
+            observation,
+            action: false,
+            reward: 0.0,
+            next_observation,
+            episode_done: next_observation.is_none(),
+        }
+    }
+    const STEP_NOT_DONE: Step<usize, bool> = step(0, Some(0));
+    const STEP_TERMINAL: Step<usize, bool> = step(0, None);
+
+    #[test]
+    fn fill_episodic() {
+        let mut buffer = SerialBufferConfig {
+            soft_threshold: 3,
+            hard_threshold: 5,
+        }
+        .build_history_buffer();
+        assert!(!buffer.push(STEP_NOT_DONE));
+        assert!(!buffer.push(STEP_TERMINAL));
+        assert!(!buffer.push(STEP_NOT_DONE)); // Soft threshold but episode not done
+        assert!(buffer.push(STEP_TERMINAL)); // Soft threshold and episode done
+    }
+
+    #[test]
+    fn fill_non_episodic() {
+        let mut buffer = SerialBufferConfig {
+            soft_threshold: 3,
+            hard_threshold: 5,
+        }
+        .build_history_buffer();
+        assert!(!buffer.push(STEP_NOT_DONE));
+        assert!(!buffer.push(STEP_NOT_DONE));
+        assert!(!buffer.push(STEP_NOT_DONE));
+        assert!(!buffer.push(STEP_NOT_DONE));
+        assert!(buffer.push(STEP_NOT_DONE)); // Hard threshold
+    }
+
+    #[fixture]
+    /// A buffer containing (in order)
+    /// * an episode of length 1,
+    /// * an episode of length 2, and
+    /// * 2 steps of an incomplete episode.
+    fn full_buffer() -> SerialBuffer<usize, bool> {
+        let mut buffer = SerialBufferConfig {
+            soft_threshold: 5,
+            hard_threshold: 6,
+        }
+        .build_history_buffer();
+        buffer.push(step(0, None));
+        buffer.push(step(1, Some(2)));
+        buffer.push(step(2, None));
+        buffer.push(step(3, Some(4)));
+        buffer.push(step(4, Some(5)));
+        buffer
+    }
+
+    #[rstest]
+    fn steps(full_buffer: SerialBuffer<usize, bool>) {
+        let mut steps_iter = full_buffer.steps();
+        assert_eq!(steps_iter.next(), Some(&step(0, None)));
+        assert_eq!(steps_iter.next(), Some(&step(1, Some(2))));
+        assert_eq!(steps_iter.next(), Some(&step(2, None)));
+        assert_eq!(steps_iter.next(), Some(&step(3, Some(4))));
+        assert_eq!(steps_iter.next(), Some(&step(4, Some(5))));
+        assert_eq!(steps_iter.next(), None);
+    }
+
+    #[rstest]
+    fn steps_is_fused(full_buffer: SerialBuffer<usize, bool>) {
+        let mut steps_iter = full_buffer.steps();
+        for _ in 0..5 {
+            assert!(steps_iter.next().is_some());
+        }
+        assert!(steps_iter.next().is_none());
+        assert!(steps_iter.next().is_none());
+    }
+
+    #[rstest]
+    fn steps_len(full_buffer: SerialBuffer<usize, bool>) {
+        assert_eq!(full_buffer.steps().len(), 5);
+    }
+
+    #[rstest]
+    fn steps_count(full_buffer: SerialBuffer<usize, bool>) {
+        assert_eq!(full_buffer.steps().count(), 5);
+    }
+
+    #[rstest]
+    fn episodes_no_incomplete(full_buffer: SerialBuffer<usize, bool>) {
+        let mut episodes_iter = full_buffer.episodes(None);
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(0, None)]
+        );
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(1, Some(2)), &step(2, None)]
+        );
+        assert!(episodes_iter.next().is_none());
+    }
+
+    #[rstest]
+    fn episodes_incomplete_ge_5(full_buffer: SerialBuffer<usize, bool>) {
+        let mut episodes_iter = full_buffer.episodes(Some(5));
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(0, None)]
+        );
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(1, Some(2)), &step(2, None)]
+        );
+        assert!(episodes_iter.next().is_none());
+    }
+
+    #[rstest]
+    fn episodes_incomplete_ge_1(full_buffer: SerialBuffer<usize, bool>) {
+        let mut episodes_iter = full_buffer.episodes(Some(1));
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(0, None)]
+        );
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(1, Some(2)), &step(2, None)]
+        );
+        assert_eq!(
+            episodes_iter.next().unwrap().iter().collect::<Vec<_>>(),
+            vec![&step(3, Some(4)), &step(4, Some(5))]
+        );
+        assert!(episodes_iter.next().is_none());
     }
 }
