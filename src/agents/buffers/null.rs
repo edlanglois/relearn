b53cee8cb1e07b18759347557d8f0b1e338f9389
Eric Langlois <eric@langlois.xyz>
2022-01-13 19:31:30 -0500

    Null buffer always reports being ready
    So that combined buffers can also report being ready

--- src/agents/buffers/null.rs
+++ src/agents/buffers/null.rs
@@ -1,20 +1,20 @@
 use super::WriteHistoryBuffer;
 use crate::simulation::PartialStep;
 
-/// Buffer that drops all steps without saving. Never reports being ready.
+/// Buffer that drops all steps without saving. Always reports being ready.
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct NullBuffer;
 
 impl<O, A> WriteHistoryBuffer<O, A> for NullBuffer {
     fn push(&mut self, _step: PartialStep<O, A>) -> bool {
-        false
+        true
     }
 
     fn extend_until_ready<I>(&mut self, _steps: I) -> bool
     where
         I: IntoIterator<Item = PartialStep<O, A>>,
     {
-        false
+        true
     }
 
     fn clear(&mut self) {}
