1ad75e77b6b519fdb1d2b37f65acea00a96a1bff
Eric Langlois <eric@langlois.xyz>
2022-03-03 17:09:24 -0500

    Use term "log_probs" instead of "logits" in Categorical distribution

--- src/torch/distributions/categorical.rs
+++ src/torch/distributions/categorical.rs
@@ -5,20 +5,28 @@ use tch::{Kind, Tensor};
 /// Categorical distribution(s).
 #[derive(Debug, PartialEq)]
 pub struct Categorical {
-    /// Log probability of each event.
+    /// Normalized log probability of each outcome.
     ///
     /// An f64 tensor of shape `[BATCH_SHAPE.., NUM_EVENTS]`.
-    logits: Tensor,
+    ///
+    /// Note: PyTorch refers to these as `logits` but I am not sure that is correct,
+    /// or at least it is confusing given the conventional meaning of Bernoulli logits.
+    /// These are `log(p_i)` but Bernoulli logits are `log(p_i / (1 - p_i))`.
+    /// The multinomial equivalent of logit parameterization omits one of the redundant log probs
+    /// (say 0) and uses `log(p_i / p_0)`.
+    /// For example, see
+    /// <https://en.wikipedia.org/wiki/Multinomial_logistic_regression#As_a_set_of_independent_binary_regressions>
+    log_probs: Tensor,
 }
 
 impl Categorical {
-    /// Initialze from possibly unnormalized log probabilities.
+    /// Initialze from unnormalized log probabilities.
     ///
-    /// The log probabilities are normalized by adding some value `C` to each
-    /// such that `sum_i exp(log_prob[i] + C) = 1`.
-    pub fn new(logits: &Tensor) -> Self {
+    /// The unnormalized input log probabilities `log(q_i)` are normalized as
+    /// `log(p_i) = log(q_i / sum_i(q_i))`.
+    pub fn new(unnormalized_log_probs: &Tensor) -> Self {
         Self {
-            logits: logits.log_softmax(-1, Kind::Float),
+            log_probs: unnormalized_log_probs.log_softmax(-1, Kind::Float),
         }
     }
 }
@@ -34,7 +42,7 @@ fn clamp_float_min(x: &Tensor) -> Result<Tensor, Kind> {
 
 impl ArrayDistribution<Tensor, Tensor> for Categorical {
     fn batch_shape(&self) -> Vec<usize> {
-        self.logits
+        self.log_probs
             .size() // shape as i64
             .split_last() // exclude NUM_EVENTS dim
             .unwrap()
@@ -49,27 +57,29 @@ impl ArrayDistribution<Tensor, Tensor> for Categorical {
     }
 
     fn sample(&self) -> Tensor {
-        self.logits.exp().multinomial(1, true).squeeze_dim(-1)
+        self.log_probs.exp().multinomial(1, true).squeeze_dim(-1)
     }
 
     fn log_probs(&self, elements: &Tensor) -> Tensor {
-        self.logits
+        self.log_probs
             .gather(-1, &elements.unsqueeze(-1), false)
             .squeeze_dim(-1)
     }
 
     fn entropy(&self) -> Tensor {
-        let clamped_logits = clamp_float_min(&self.logits)
-            .map_err(|kind| format!("logits must be f32 or f64, not {:?}", kind))
+        // Clamping avoids -INF * exp(-INF) = -INF * 0 = NaN
+        let clamped_log_probs = clamp_float_min(&self.log_probs)
+            .map_err(|kind| format!("log_probs must be f32 or f64, not {:?}", kind))
             .unwrap();
-        -(clamped_logits * self.logits.exp()).sum_dim_intlist(&[-1], false, Kind::Float)
+        -(clamped_log_probs * self.log_probs.exp()).sum_dim_intlist(&[-1], false, Kind::Float)
     }
 
     fn kl_divergence_from(&self, other: &Self) -> Tensor {
-        let clamped_rel_logits = clamp_float_min(&(&self.logits - &other.logits))
-            .map_err(|kind| format!("logits must be f32 or f64, not {:?}", kind))
+        // Clamping avoids -INF * exp(-INF) = -INF * 0 = NaN
+        let clamped_rel_log_probs = clamp_float_min(&(&self.log_probs - &other.log_probs))
+            .map_err(|kind| format!("log_probs must be f32 or f64, not {:?}", kind))
             .unwrap();
-        (clamped_rel_logits * self.logits.exp()).sum_dim_intlist(&[-1], false, Kind::Float)
+        (clamped_rel_log_probs * self.log_probs.exp()).sum_dim_intlist(&[-1], false, Kind::Float)
     }
 }
 
@@ -80,38 +90,38 @@ mod tests {
 
     #[test]
     fn batch_shape_0d() {
-        let logits = Tensor::of_slice(&[0.0_f32, 1.0, 0.0]).log();
-        let d = Categorical::new(&logits);
+        let log_probs = Tensor::of_slice(&[0.0_f32, 1.0, 0.0]).log();
+        let d = Categorical::new(&log_probs);
         assert_eq!(d.batch_shape(), [] as [usize; 0]);
     }
 
     #[test]
     fn batch_shape_1d() {
-        let logits = Tensor::of_slice(&[
+        let log_probs = Tensor::of_slice(&[
             0.0_f32, 1.0, 0.0, //
             0.2, 0.3, 0.5, //
         ])
         .reshape(&[2, 3])
         .log();
-        let d = Categorical::new(&logits);
+        let d = Categorical::new(&log_probs);
         assert_eq!(d.batch_shape(), [2]);
     }
 
     #[test]
     fn element_shape() {
-        let logits = Tensor::of_slice(&[
+        let log_probs = Tensor::of_slice(&[
             0.0_f32, 1.0, 0.0, //
             0.2, 0.3, 0.5, //
         ])
         .reshape(&[2, 3])
         .log();
-        let d = Categorical::new(&logits);
+        let d = Categorical::new(&log_probs);
         assert_eq!(d.element_shape(), [] as [usize; 0]);
     }
 
     #[test]
     fn sample() {
-        let logits = Tensor::of_slice(&[
+        let log_probs = Tensor::of_slice(&[
             1.0_f32, 0.0, 0.0, //
             0.0, 1.0, 0.0, //
             0.0, 0.0, 1.0, //
@@ -119,7 +129,7 @@ mod tests {
         ])
         .reshape(&[4, 3])
         .log();
-        let d = Categorical::new(&logits);
+        let d = Categorical::new(&log_probs);
         let samples = d.sample();
         assert_eq!(samples.size(), [4]);
         assert_eq!(samples.i(..3), Tensor::of_slice(&[0_i64, 1, 2]));
@@ -128,7 +138,7 @@ mod tests {
 
     #[test]
     fn log_probs() {
-        let logits = Tensor::of_slice(&[
+        let log_probs = Tensor::of_slice(&[
             // elem: 1
             f32::NEG_INFINITY,
             0.0,
@@ -163,7 +173,7 @@ mod tests {
             0.0,
         ])
         .reshape(&[-1, 3]);
-        let distribution = Categorical::new(&logits);
+        let distribution = Categorical::new(&log_probs);
 
         let elements = Tensor::of_slice(&[1_i64, 0, 2, 0, 0, 1, 2, 0]);
 
@@ -192,7 +202,7 @@ mod tests {
 
     #[test]
     fn entropies() {
-        let logits = Tensor::of_slice(&[
+        let log_probs = Tensor::of_slice(&[
             f32::NEG_INFINITY,
             0.0,
             f32::NEG_INFINITY,
@@ -210,7 +220,7 @@ mod tests {
             0.6_f32.ln(),
         ])
         .reshape(&[-1, 3]);
-        let distribution = Categorical::new(&logits);
+        let distribution = Categorical::new(&log_probs);
 
         let actual = distribution.entropy();
         let expected = Tensor::of_slice(&[
@@ -230,23 +240,23 @@ mod tests {
 
     #[test]
     fn kl_divergence() {
-        let logits_a = Tensor::of_slice(&[
+        let log_probs_a = Tensor::of_slice(&[
             0.2_f32, 0.3, 0.5, //
             0.2, 0.3, 0.5, //
             0.0, 1.0, 0.0, //
         ])
         .reshape(&[3, 3])
         .log();
-        let distribution_a = Categorical::new(&logits_a);
+        let distribution_a = Categorical::new(&log_probs_a);
 
-        let logits_b = Tensor::of_slice(&[
+        let log_probs_b = Tensor::of_slice(&[
             0.2_f32, 0.3, 0.5, //
             0.7, 0.2, 0.1, //
             0.2, 0.3, 0.5, //
         ])
         .reshape(&[3, 3])
         .log();
-        let distribution_b = Categorical::new(&logits_b);
+        let distribution_b = Categorical::new(&log_probs_b);
 
         let actual = distribution_a.kl_divergence_from(&distribution_b);
         let expected = Tensor::of_slice(&[
