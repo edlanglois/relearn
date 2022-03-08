c274e5405b97a50ab4c1697ecae8bd07e8d61475
Eric Langlois <eric@langlois.xyz>
2022-03-08 17:59:42 -0500

    Global CUDA setup for tests; avoids slow setup in each tests

--- src/torch/mod.rs
+++ src/torch/mod.rs
@@ -9,3 +9,22 @@ pub mod modules;
 pub mod optimizers;
 pub mod updaters;
 pub mod utils;
+
+/// Initialization function for unit tests using CUDA.
+///
+/// Creating a Tensor on CUDA for the first time on each thread triggers a long (~1s) setup
+/// routine.
+/// Each test runs in its own thread so this setup happens many times (albeit concurrently).
+/// With [`ctor`], this function automatically runs immediately before per-test threads are spawned
+/// and creates a small CUDA tensor (if CUDA is available). Consequently, the initialization is
+/// only performed once. The test threads spawn with the setup already in effect.
+///
+#[cfg(test)]
+#[ctor::ctor]
+fn cuda_test_init() {
+    // Create a small CUDA tensor to trigger the slow CUDA setup routine.
+    if tch::Cuda::is_available() {
+        println!("Initializing CUDA for PyTorch before starting tests...");
+        let _ = tch::Tensor::zeros(&[1], (tch::Kind::Int, tch::Device::Cuda(0)));
+    }
+}
