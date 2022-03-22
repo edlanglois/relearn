c85a1c6d70c8d7c02b54fae379ba87204f2d57bb
Eric Langlois <eric@langlois.xyz>
2022-03-22 03:07:59 -0400

    Split StepsSummary / OnlineStepsSummary

--- src/simulation/summary.rs
+++ src/simulation/summary.rs
@@ -1,17 +1,21 @@
 use super::PartialStep;
 use crate::utils::stats::OnlineMeanVariance;
+use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::iter::FromIterator;
 
 /// Summary statistics of simulation steps.
-#[derive(Debug, Default, Copy, Clone, PartialEq)]
+#[derive(Debug, Default, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub struct StepsSummary {
     pub step_reward: OnlineMeanVariance<f64>,
     pub episode_reward: OnlineMeanVariance<f64>,
     pub episode_length: OnlineMeanVariance<f64>,
+}
 
-    current_episode_reward: f64,
-    current_episode_length: u64,
+impl From<OnlineStepsSummary> for StepsSummary {
+    fn from(online_summary: OnlineStepsSummary) -> Self {
+        online_summary.completed
+    }
 }
 
 impl fmt::Display for StepsSummary {
@@ -27,18 +31,6 @@ impl fmt::Display for StepsSummary {
 }
 
 impl StepsSummary {
-    pub fn push<O, A>(&mut self, step: &PartialStep<O, A>) {
-        self.step_reward.push(step.reward);
-        self.current_episode_reward += step.reward;
-        self.current_episode_length += 1;
-        if step.next.episode_done() {
-            self.episode_reward.push(self.current_episode_reward);
-            self.current_episode_reward = 0.0;
-            self.episode_length.push(self.current_episode_length as f64);
-            self.current_episode_length = 0;
-        }
-    }
-
     #[inline]
     pub fn num_steps(&self) -> u64 {
         self.step_reward.count()
@@ -51,6 +43,51 @@ impl StepsSummary {
 }
 
 impl<O, A> FromIterator<PartialStep<O, A>> for StepsSummary {
+    fn from_iter<I>(steps: I) -> Self
+    where
+        I: IntoIterator<Item = PartialStep<O, A>>,
+    {
+        OnlineStepsSummary::from_iter(steps).into()
+    }
+}
+
+/// Online calculation of simulation step statistics.
+#[derive(Debug, Default, Copy, Clone, PartialEq, Serialize, Deserialize)]
+pub struct OnlineStepsSummary {
+    completed: StepsSummary,
+    current_episode_reward: f64,
+    current_episode_length: u64,
+}
+
+impl OnlineStepsSummary {
+    pub fn push<O, A>(&mut self, step: &PartialStep<O, A>) {
+        self.completed.step_reward.push(step.reward);
+        self.current_episode_reward += step.reward;
+        self.current_episode_length += 1;
+        if step.next.episode_done() {
+            self.completed
+                .episode_reward
+                .push(self.current_episode_reward);
+            self.current_episode_reward = 0.0;
+            self.completed
+                .episode_length
+                .push(self.current_episode_length as f64);
+            self.current_episode_length = 0;
+        }
+    }
+
+    #[inline]
+    pub fn num_steps(&self) -> u64 {
+        self.completed.num_steps()
+    }
+
+    #[inline]
+    pub fn num_episodes(&self) -> u64 {
+        self.completed.num_episodes()
+    }
+}
+
+impl<O, A> FromIterator<PartialStep<O, A>> for OnlineStepsSummary {
     fn from_iter<I>(steps: I) -> Self
     where
         I: IntoIterator<Item = PartialStep<O, A>>,
