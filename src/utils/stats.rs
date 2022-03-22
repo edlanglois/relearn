b231550053856a0727534bb641ea1d7efa162836
Eric Langlois <eric@langlois.xyz>
2022-03-22 02:46:17 -0400

    Impl Add, AddAssign and other agg. methods for OnlineMeanVariance

--- src/utils/stats.rs
+++ src/utils/stats.rs
@@ -2,6 +2,7 @@
 use num_traits::{real::Real, Zero};
 use std::fmt;
 use std::iter::{Extend, FromIterator};
+use std::ops::{Add, AddAssign};
 
 /// Online mean and variance calculation using Welford's Algorithm
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -98,6 +99,29 @@ impl<T: Real> OnlineMeanVariance<T> {
         let residual_post = value - self.mean;
         self.squared_residual_sum = self.squared_residual_sum + residual_pre * residual_post;
     }
+
+    /// Calculate mean and variance of a slice using numerically-stable divide-and-conquer
+    pub fn from_slice(data: &[T]) -> Self {
+        if data.len() <= 8 {
+            return data.iter().cloned().collect();
+        }
+        let mid = data.len() / 2;
+        Self::from_slice(&data[..mid]) + Self::from_slice(&data[mid..])
+    }
+
+    /// Aggregate mean and variance of slice of `OnlineMeanVariance` instances.
+    ///
+    /// Uses a numerically-stable divide-and-conquer approach.
+    pub fn from_stats(stats: &[Self]) -> Self {
+        match stats {
+            [] => Self::default(),
+            [x] => *x,
+            _ => {
+                let mid = stats.len();
+                Self::from_stats(&stats[..mid]) + Self::from_stats(&stats[mid..])
+            }
+        }
+    }
 }
 
 impl<T: Real> Extend<T> for OnlineMeanVariance<T> {
@@ -122,6 +146,41 @@ impl<T: Real> FromIterator<T> for OnlineMeanVariance<T> {
     }
 }
 
+impl<T: Real> Add for OnlineMeanVariance<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        // Reference
+        // Algorithms for computing the sample variance: analysis and recommendations
+        // Chan et al. (1979)
+        // <http://www.cs.yale.edu/publications/techreports/tr222.pdf>
+        // <https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm>
+        let self_count = T::from(self.count).unwrap();
+        let other_count = T::from(other.count).unwrap();
+        let count = self.count + other.count;
+        let total_count = T::from(count).unwrap();
+
+        let mean = (self.mean * self_count + other.mean * other_count) / total_count;
+        let delta = self.mean - other.mean;
+        let squared_residual_sum = self.squared_residual_sum
+            + other.squared_residual_sum
+            + delta * delta * self_count * other_count / total_count;
+
+        Self {
+            mean,
+            squared_residual_sum,
+            count,
+        }
+    }
+}
+
+impl<T: Real> AddAssign for OnlineMeanVariance<T> {
+    #[inline]
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -132,5 +191,29 @@ mod tests {
             [1.0, 2.0, 3.0, 4.0].into_iter().map(Into::into).collect();
         assert!((stats.mean().unwrap() - 2.5).abs() < 1e-8);
         assert!((stats.variance().unwrap() - 1.25).abs() < 1e-8);
+    }
+
+    #[test]
+    fn from_slice() {
+        let data: Vec<_> = (0..20u8).map(f32::from).collect();
+        let collected: OnlineMeanVariance<_> = data.iter().cloned().collect();
+        assert_eq!(OnlineMeanVariance::from_slice(&data), collected);
+    }
+
+    #[test]
+    fn add() {
+        let a: OnlineMeanVariance<f64> = [1.0, 2.0].into_iter().collect();
+        let b: OnlineMeanVariance<f64> = [3.0, 4.0].into_iter().collect();
+        let c: OnlineMeanVariance<f64> = [1.0, 2.0, 3.0, 4.0].into_iter().collect();
+        assert_eq!(a + b, c);
+    }
+
+    #[test]
+    fn add_assign() {
+        let a: OnlineMeanVariance<f64> = [1.0, 2.0].into_iter().collect();
+        let mut b: OnlineMeanVariance<f64> = [3.0, 4.0].into_iter().collect();
+        b += a;
+        let c: OnlineMeanVariance<f64> = [1.0, 2.0, 3.0, 4.0].into_iter().collect();
+        assert_eq!(b, c);
     }
 }
